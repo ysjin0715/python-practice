10e524dea9a16e8c79e43ce73dc4b4674640aea3
YangSeungJin <ysjin0715@gmail.com>
2019-12-03 21:52:17 +0900

    Turtle and lab

---
+++ chapter1/lab2.py
@@ -0,0 +1,27 @@
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+# t.left(120)
+# t.forward(100)
+# time.sleep(3)
+
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+
+t.circle(100)
+time.sleep(3)
---
+++ chapter1/turtle1.py
@@ -0,0 +1,9 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.left(90)
+t.forward(200)
+
+time.sleep(10)
