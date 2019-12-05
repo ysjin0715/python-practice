fca7d860646e037abe8fe371da5d022e9c71f908
YangSeungJin <ysjin0715@gmail.com>
2019-12-05 15:31:55 +0900

    practice1-9

---
+++ chapter1/practice1-3.py
@@ -0,0 +1,101 @@
+#1.
+print("환영합니다.")
+print("파이썬의","세계에","오신","것을","환영합니다")
+print("파이썬은 강력합니다.")
+
+#2.
+#반갑습니다. 파이썬!
+#0.6
+#Hello World !!!
+
+#3.
+print(7*24)
+
+#4.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+time.sleep(1)
+
+#5.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+t.width(10)
+t.forward(100)
+t.left(90)
+t.forward(100)
+time.sleep(1)
+
+#6.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+t.color("blue")
+t.left(180)
+t.forward(100)
+time.sleep(1)
+
+#7.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("square")
+t.right(90)
+t.forward(100)
+time.sleep(1)
+
+#8.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+t.up()
+t.goto(100,200)
+t.down()
+t.forward(100)
+t.up()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.down()
+t.forward(100)
+time.sleep(1)
+
+#9.
+import turtle
+import time
+t=turtle.Turtle()
+t.shape("turtle")
+t.width(3)
+t.color("red")
+t.circle(100)
+t.up()
+t.goto(-95,-140)
+t.down()
+t.circle(100)
+t.up()
+t.goto(-190,0)
+t.down()
+t.circle(100)
+t.up()
+t.goto(95,-140)
+t.down()
+t.circle(100)
+t.up()
+t.goto(190,0)
+t.down()
+t.circle(100)
+time.sleep(1)
