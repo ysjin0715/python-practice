51c056de3e2f50ef752d89893506e928047949cd
YangSeungJin <ysjin0715@gmail.com>
2019-12-13 20:11:56 +0900

    chapter4 str,int,float

---
+++ chapter4/lab.py
@@ -0,0 +1,38 @@
+import turtle
+t=turtle.Turtle()
+t.shape="turtle"
+
+#1
+s=turtle.textinput('','이름을 입력하십시오: ')
+t.write('안녕하세요? %s씨, 터틀 인사드립니다.' %s)
+a=4
+
+for i in range(a):
+    t.fd(100)
+    t.left(90)
+
+2.
+print('안녕하세요?')
+name=input('이름이 어떻게 되시나요?')
+
+print('만나서 반갑습니다 %s씨'%name)
+print('이름의 길이는 다음과 같군요: ',len(name))
+
+age=int(input('나이가 어떻게 되나요?'))
+print('내년이면 '+str(age+1)+ ' 이 되시는군요.')
+
+#3.
+year=int(input('오늘의 연도를 입력하시오: '))
+month=int(input('오늘의 월을 입력하시오: '))
+day=int(input('오늘의 일을 입력하시오: '))
+print('오늘은',str(year),'년 ',str(month),'월 ',str(day),'일입니다.')
+
+#4.
+import time
+now=time.time()
+thisYear=int(1970+now//(365*24*3600))
+print('올해는 ',str(thisYear),'입니다.')
+age=int(input('몇 살이신지요? '))
+future=2050-thisYear
+fage=future+age
+print('2050년에는',str(fage),'살 이시군요.')
---
+++ chapter4/study.py
@@ -0,0 +1,81 @@
+#2.
+x=10
+print('x=',x)
+x=3.14
+print('x=',x)
+x='hello world!'
+print('x=',x)
+
+#4.
+msg='hello'
+print(msg)
+
+msg="hello"
+print(msg)
+
+#6.
+msg="철수가 '안녕'이라고 말했습니다."
+print(msg)
+
+#7.
+print(100+200)
+print("100"+"200")
+
+x=input('정수를 입력하시오: ')
+y=input('정수를 입력하시오: ')
+print(x+y)
+
+#8.
+t=input('정수를 입력하시오: ')
+a=int(t)
+
+t=input('정수를 입력하시오: ')
+b=int(t)
+
+print(a+b)
+
+type("hello world")
+type(3.2)
+type(17)
+
+#9.
+t=input('정수를 입력하시오: ')
+x=float(t)
+t=input('정수를 입력하시오: ')
+y=float(t)
+
+print(x+y)
+
+#10.
+print('나는 현재'+str(21)+'살이다.')
+print('원주율은 '+str(3.14)+'입니다.')
+
+
+#11.
+first_name="길동"
+last_name='홍'
+name=last_name+first_name
+print(name)
+
+#12.
+line="="*50
+print(line)
+
+msg="Congratulations!"
+print(msg*3)
+
+price=10000
+print('상품의 가격은 %s원입니다.'%price)
+
+
+#13.
+s='monty python'
+print(s[6:10])
+
+#14.
+print('말 한마디로\n천 냥 빚을 갚는다')
+print('말 한마디로\t천 냥 빚을 갚는다')
+print('말 한마디로\\천 냥 빚을 갚는다')
+print('말 한마디로\"천 냥 빚을 갚는다')
+print('말 한마디로\'천 냥 빚을 갚는다')
+print('doesn\'t')
