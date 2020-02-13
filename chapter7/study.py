c84cc781a9da6a268d613a7a515741fa955054e7
ysjin0715 <ysjin0715@gmail.com>
2020-02-14 00:36:10 +0900

    함수

--- chapter7/study.py
+++ chapter7/study.py
@@ -1,27 +1,51 @@
-#3-4.함수 설정하기
+# 3-4.함수 설정하기
 def print_address():
     print('서울특별시 종로구 1번지')
     print('파이썬 빌딩 7층')
     print('홍길동')
 
+
 print_address()
 print_address()
 print_address()
 
-#5.인수(홍길동)-매개변수(name변수) 간 관계 이해하기
+
+# 5.인수(홍길동)-매개변수(name변수) 간 관계 이해하기
 def print_address(name):
     print('서울특별시 종로구 1번지')
     print('파이썬 빌딩 7층')
     print(name)
 
+
 print_address('홍길동')
 
-#6. 반환 값 알아보기
-def calculate_area (radius):
-    area=3.14*radius**2
-    return(area)
-c_area=calculate_area(5.0)
-print(c_area)
-area_sum=calculate_area(5.0)+calculate_area(10,0)
 
-#7.
+# 6. 반환 값 알아보기
+def calculate_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+
+c_area = calculate_area(5.0)
+print(c_area)
+area_sum = calculate_area(5.0) + calculate_area(10.0)
+print(area_sum)
+
+
+# 7. 여러개의 값 반환하기
+def git_input():
+    return 2, 3
+
+
+x, y = git_input()
+print(x, y)
+
+
+#7. 함수에 여러 개의 입력 전달하기
+def get_sum(start, end):
+    sum=0
+    for i in range(start, end+1):
+        sum+=i
+    return sum
+
+print(get_sum(1,10))
